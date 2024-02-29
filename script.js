5e3f746d561b7eb35f0ef38c662c86a8fa2700bd
hishamrehab <hishamrehab309@gmail.com>
2024-02-29 12:34:29 +0200

    form validation

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var form = `<div>
+
   <div class="form-group">
     <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" placeholder="Enter Your Name">
+    <input type="text" class="form-control" id="name" placeholder="Enter Your Name" required>
   </div>
+
+  <div class="form-group mt-3 mb-3">
+    <label for="age">Age</label>
+    <input type="age" class="form-control" id="age" placeholder="Enter Your Age" required>
+  </div
+
   <div class="form-group mt-3">
     <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" placeholder="Enter Your Email">
+    <input type="email" class="form-control" id="email" placeholder="Enter Your Email" required>
   </div>
-  
+
 
   <button type="submit" class="btn btn-primary mt-4" onclick="save()">Submit</button>
 </div>`;
@@ -18,6 +25,7 @@ function table() {
     <tr>
       <th class="col-1">NO</th>
       <th class="col-2">Name</th>
+      <th class="col-2">Age</th>
       <th class="col-3">Email</th>
       <th class="col-4">Edit</th>
       <th class="col-5">Delete</th>
@@ -32,6 +40,7 @@ function table() {
       ` <tr>
       <th class="col-1">${i + 1}</th>
       <td class="col-2">${details[i].name}</td>
+      <td class="col-2">${details[i].age}</td>
       <td class="col-3">${details[i].email}</td>
       <td class="col-4">
        <button type="button" class="btn btn-warning mt-3" onclick="edit(${i})" >Edit</button>
@@ -69,15 +78,42 @@ function setData() {
 
 const save = () => {
   let name = document.getElementById("name");
+  let age = document.getElementById("age");
   let email = document.getElementById("email");
 
   if (name.value == "") {
-    alert("name is empty");
+    alert("name is required");
+    return;
+  }
+
+  if (age.value == "") {
+    alert("age is required");
+    return;
+  }
+
+  if (age.value < 18) {
+    alert("age is not valid");
+    return;
+  }
+
+  if (age.value > 30) {
+    alert("age is not valid");
+    return;
+  }
+
+  if (email.value == "") {
+    alert("Email is required");
+    return;
+  }
+
+  if (!email.value.includes("@")) {
+    alert("Email is not vaild");
     return;
   }
 
   let data = {
     name: name.value,
+    age: age.value,
     email: email.value,
   };
   details.push(data);
@@ -87,6 +123,7 @@ const save = () => {
   setData();
   table();
   name.value = "";
+  age.value = "";
   email.value = "";
 };
 
@@ -102,12 +139,23 @@ function edit(index) {
   var editForm = `<div>
   <div class="form-group">
     <label for="name">Update Name</label>
-    <input   value="${details[index].name}" type="text" class="form-control" id="newName" placeholder="Enter Your Updated Name">
+    <input   value="${details[index].name}" type="text" class="form-control" id="newName" placeholder="Enter Your Updated Name" required>
   </div>
+
+  
+  <div class="form-group mt-3 mb-3">
+    <label for="age">Age</label>
+    <input value="${details[index].age}" type="age" class="form-control" id="newAge" placeholder="Enter New Age" required>
+  </div
+
+
+
   <div class="form-group mt-3">
     <label for="email">Email</label>
-    <input  value="${details[index].email}"  type="email" class="form-control" id="newEmail" placeholder="Enter Your Updated Email">
+    <input  value="${details[index].email}"  type="email" class="form-control" id="newEmail" placeholder="Enter Your Updated Email" required>
   </div>
+
+
 
   <button type="submit" class="btn btn-primary mt-4" onclick="update(${index})">Update</button>
 </div>`;
@@ -117,10 +165,12 @@ function edit(index) {
 
 function update(index) {
   let newName = document.getElementById("newName");
+  let newAge = document.getElementById("newAge");
   let newEmail = document.getElementById("newEmail");
 
   details[index] = {
     name: newName.value,
+    age: newAge.value,
     email: newEmail.value,
   };
   setData();
